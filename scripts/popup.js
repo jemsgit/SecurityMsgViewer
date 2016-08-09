4ed0d6ba3d43fea1867f1b186fe2978080f5a82f
Aleksey Cherepanov <ftp27host@gmail.com>
2016-08-09 11:55:14 +0300

    Add localization support

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,27 @@
-﻿(function ($) {
+﻿function localizeHtmlPage()
+{
+    //Localize by replacing __MSG_***__ meta tags
+    //From: http://stackoverflow.com/questions/25467009/internationalization-of-html-pages-for-my-google-chrome-extension
+    var objects = document.getElementsByTagName('html');
+    for (var j = 0; j < objects.length; j++)
+    {
+        var obj = objects[j];
+
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1)
+        {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
+
+        if(valNewH != valStrH)
+        {
+            obj.innerHTML = valNewH;
+        }
+    }
+}
+
+
+(function ($) {
     $(document).ready(function () {
         var localStorageId = 'SecurityMessageViewer',
             iFrame = '<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account=410013247085243&quickpay=shop&payment-type-choice=on&mobile-payment-type-choice=on&writer=seller&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%B0%D1%82%D1%8C&targets-hint=&default-sum=20&button-text=03&successURL=" width="450" height="198"></iframe>';
@@ -37,12 +60,11 @@
             setTextAndIcon(that.checked);
         })
 
-        
-
-
         $('.donate_link').on('click', function(event){
             $('.yandex_money_form').toggleClass('active')
         })
+
+        localizeHtmlPage();
     })
 
 })(jQuery)
